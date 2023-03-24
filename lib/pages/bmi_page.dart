8ce43f7c445d7d899795997d10f4eabde546b651
kiet <truongthekiet020199@gmail.com>
2023-03-24 17:39:58 +0700

    update full

--- lib/pages/bmi_page.dart
+++ lib/pages/bmi_page.dart
@@ -1,6 +1,9 @@
+import 'dart:math';
+
 import 'package:bmi_app/cards/info_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class BMIPage extends StatefulWidget {
   const BMIPage({Key? key}) : super(key: key);
@@ -10,21 +13,35 @@ class BMIPage extends StatefulWidget {
 }
 
 class _BMIPageState extends State<BMIPage> {
-  int age = 20;
+  int age = 20, weight = 80, height = 170, gender = 0;
   double? deviceHeight, deviceWidth;
   @override
   Widget build(BuildContext context) {
     deviceHeight = MediaQuery.of(context).size.height;
     deviceWidth = MediaQuery.of(context).size.width;
     return CupertinoPageScaffold(
-        child: Center(
-            child: InfoCard(
-                width: deviceWidth! * 0.45,
-                height: deviceHeight! * 0.18,
-                child: ageSelect())));
+        child: SizedBox(
+      height: deviceHeight! * 0.75,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [_ageSelect(), _weightSelect()],
+          ),
+          _heightSelect(),
+          _genderSelecct(),
+          _calculatateBmiButton(),
+        ],
+      ),
+    ));
   }
 
-  Widget ageSelect() {
+  Widget _ageSelect() {
     return InfoCard(
         width: deviceWidth! * 0.45,
         height: deviceHeight! * 0.18,
@@ -33,11 +50,11 @@ class _BMIPageState extends State<BMIPage> {
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            const Text('Age',
+            const Text('Age-yr',
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
             Text(
               age.toString(),
-              style: TextStyle(fontSize: 45, fontWeight: FontWeight.w700),
+              style: const TextStyle(fontSize: 45, fontWeight: FontWeight.w700),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
@@ -74,5 +91,164 @@ class _BMIPageState extends State<BMIPage> {
             )
           ],
         ));
+  }
+
+  Widget _weightSelect() {
+    return InfoCard(
+        width: deviceWidth! * 0.45,
+        height: deviceHeight! * 0.18,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text('Weight-kg',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400)),
+            Text(
+              weight.toString(),
+              style: const TextStyle(fontSize: 45, fontWeight: FontWeight.w700),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(
+                    width: 50,
+                    child: CupertinoDialogAction(
+                      onPressed: () {
+                        setState(() {
+                          weight--;
+                        });
+                      },
+                      child: const Text(
+                        '-',
+                        style: TextStyle(fontSize: 25, color: Colors.red),
+                      ),
+                    )),
+                SizedBox(
+                    width: 50,
+                    child: CupertinoDialogAction(
+                      onPressed: () {
+                        setState(() {
+                          weight++;
+                        });
+                      },
+                      child: const Text(
+                        '+',
+                        style: TextStyle(fontSize: 25, color: Colors.red),
+                      ),
+                    ))
+              ],
+            )
+          ],
+        ));
+  }
+
+  Widget _heightSelect() {
+    return InfoCard(
+        width: deviceWidth! * 0.9,
+        height: deviceHeight! * 0.18,
+        child:
+            Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+          const Text(
+            'Height-cm',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w400),
+          ),
+          Text(
+            height.toString(),
+            style: const TextStyle(fontSize: 45, fontWeight: FontWeight.w700),
+          ),
+          CupertinoSlider(
+            min: 30,
+            max: 300,
+            divisions: 300,
+            value: height.toDouble(),
+            onChanged: ((value) {
+              setState(() {
+                height = value.toInt();
+              });
+            }),
+          )
+        ]));
+  }
+
+  Widget _genderSelecct() {
+    return InfoCard(
+        width: deviceWidth! * 0.9,
+        height: deviceHeight! * 0.15,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Text(
+              'Gender',
+              style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+            ),
+            CupertinoSlidingSegmentedControl(
+                groupValue: gender,
+                children: const {0: Text('male'), 1: Text('female')},
+                onValueChanged: ((value) {
+                  setState(() {
+                    gender = value as int;
+                  });
+                }))
+          ],
+        ));
+  }
+
+  Widget _calculatateBmiButton() {
+    return SizedBox(
+      height: deviceHeight! * 0.07,
+      child: CupertinoButton.filled(
+          child: const Text('Calculate BMI'),
+          onPressed: () {
+            if (height > 0 && weight > 0 && age > 0) {
+              double bmi = weight / pow(height / 100, 2);
+              _showResultDialog(bmi);
+            }
+          }),
+    );
+  }
+
+  void _showResultDialog(double bmi) {
+    String? status;
+    if (bmi < 18.5) {
+      status = 'UnderWeight';
+    }
+    if (18.5 <= bmi && bmi < 25) {
+      status = 'Nomal';
+    }
+    if (bmi < 30 && bmi >= 25) {
+      status = 'OverWeight';
+    }
+    if (bmi >= 30) {
+      status = 'Obese';
+    }
+    showCupertinoDialog(
+        context: context,
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: Text(status!),
+            content: Text(bmi.toStringAsFixed(2)),
+            actions: [
+              CupertinoDialogAction(
+                onPressed: (() {
+                  _saveResult(bmi.toString(), status!);
+                  Navigator.pop(context);
+                }),
+                child: const Text('OK'),
+              )
+            ],
+          );
+        });
+  }
+
+  void _saveResult(String bmi, String status) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('bmi_date', DateTime.now().toString());
+    await prefs.setStringList('bmi_data', <String>[bmi, status]);
+    print('BMI result saved');
   }
 }
--- lib/pages/history_page.dart
+++ lib/pages/history_page.dart
@@ -1,14 +1,71 @@
+import 'package:bmi_app/cards/info_card.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HistoryPage extends StatelessWidget {
-  const HistoryPage({Key? key}) : super(key: key);
-
+  HistoryPage({Key? key}) : super(key: key);
+  double? deviceHeight, deviceWidth;
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-        child: Container(
-      color: Colors.lightBlue,
-    ));
+    deviceHeight = MediaQuery.of(context).size.height;
+    deviceWidth = MediaQuery.of(context).size.width;
+
+    return CupertinoPageScaffold(child: _dataCard());
+  }
+
+  Widget _dataCard() {
+    return FutureBuilder(
+        future: SharedPreferences.getInstance(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            final prefs = snapshot.data as SharedPreferences;
+            final date = prefs.getString('bmi_date');
+            final data = prefs.getStringList('bmi_data');
+            return Center(
+              child: InfoCard(
+                  width: deviceWidth! * 0.75,
+                  height: deviceHeight! * 0.25,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      _statusText(data![1]),
+                      _dateText(date!),
+                      _bmiText(data[0])
+                    ],
+                  )),
+            );
+          } else {
+            return const Center(
+              child: CupertinoActivityIndicator(
+                color: Colors.lightBlue,
+              ),
+            );
+          }
+        });
+  }
+
+  Widget _statusText(String status) {
+    return Text(
+      status,
+      style: const TextStyle(fontSize: 30, fontWeight: FontWeight.w400),
+    );
+  }
+
+  Widget _dateText(String date) {
+    DateTime parsedDate = DateTime.parse(date);
+    return Text(
+      '${parsedDate.day}/${parsedDate.month}/${parsedDate.year}',
+      style: const TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+    );
+  }
+
+  Widget _bmiText(String bmi) {
+    return Text(
+      double.parse(bmi).toStringAsFixed(2),
+      style: const TextStyle(fontSize: 60, fontWeight: FontWeight.w700),
+    );
   }
 }
